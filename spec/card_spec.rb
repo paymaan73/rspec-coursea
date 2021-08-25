1ea3f39e93ec13a34a0bee806616f4fb7c540035
paymaan73 <paymankazemipymkz@gmail.com>
2021-08-25 16:52:45 +0430

    init

---
+++ spec/card_spec.rb
@@ -0,0 +1,13 @@
+class Card
+  attr_reader :type
+  def initialize(type)
+    @type = type
+  end
+end
+
+RSpec.describe 'Card' do
+  it 'has a type' do
+    card = Card.new('Ace of Spades')
+    expect(card.type).to eql('Ace of Spades')
+  end
+end
