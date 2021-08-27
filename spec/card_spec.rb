b96d953f8df125212c55ad72481fbf11e085f379
PaymaanKazemi <75364955+paymaan73@users.noreply.github.com>
2021-08-27 18:12:49 +0000

    use let before hooks methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
@@ -7,16 +7,13 @@ class Card
 end
 
 RSpec.describe Card do
-#  before do
-#    @card = Card.new('Ace', 'Spades')
-#  end
-
-def card
-  Card.new('Ace', 'Spades')
-end
-
-  it 'has a rank' do
+  let(:card) { Card.new('Ace', 'Spades')}
+  # let(:x) { 1 + 1 }
+  # let(:y) { x + 10 }
+  it 'has a rank and that rank can change' do
     expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
