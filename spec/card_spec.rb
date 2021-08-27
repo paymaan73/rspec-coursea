aeda93ae97e953c049c92bf097bf78ec051cf92d
PaymaanKazemi <75364955+paymaan73@users.noreply.github.com>
2021-08-27 07:59:57 +0000

    reducing and duplicate method helper before hooks

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,15 +7,20 @@ class Card
 end
 
 RSpec.describe Card do
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+#  before do
+#    @card = Card.new('Ace', 'Spades')
+#  end
+
+def card
+  Card.new('Ace', 'Spades')
+end
+
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 
 end
